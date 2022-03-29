8d9f1001e0447bd4798f44115bc630427cb48db7
masao8231 <mm.neto2007@gmail.com>
2022-03-28 22:24:51 -0300

    implementando a impressão da tela

--- main.c
+++ main.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define TAM 40 
+#define TAM 10
 
-void clearScrn(char **scrn, int linha, int col);
+void limpaTela(char **scrn, int linha, int col);
 //np = numero de palavras
 void distribuiPal(char **scrn, char **palavras, int linha, int col, int np);
-
+void inserePalavra(char *palavra, char **tela);
+void desenhaNaTela(char item, int x, int y, char **scrn);
+void atualizarTela(char **scrn, int tamx, int tamy);
 
 char lista[15][20] = {
-    "Ubequistão",
+    "Uzbequistão",
     "Malásia",
     "Europa",
     "Canadá",
@@ -18,20 +20,45 @@ char lista[15][20] = {
     "China"
 };
 
-char tela[TAM][TAM]; 
-
 
 int main (){
-    clearScrn(tela, 40, 40);
-    printf("aaaa");
+    char **tela = (char **)malloc(TAM * sizeof(char *));
+    for (int i = 0; i<TAM; i++){
+        tela[i] = (char *) malloc(TAM * sizeof(char));
+    }
+    limpaTela(tela, TAM, TAM);    
+    atualizarTela(tela, TAM, TAM);
 }
 
-void clearScrn(char **scrn, int linha, int col){
+
+
+void limpaTela(char **scrn, int linha, int col){
     for (int i=0; i<linha; i++){
         for (int j=0; j<col; j++){
-            scrn[i][j] = ' '; 
+            //printf("inseriu linha: %d, coluna: %d\n", i, j);
+            scrn[i][j] = '#'; 
         }
     }
+}
+
+void desenhaNaTela(char item, int x, int y, char **scrn){
+    scrn[x][y] = item; 
+}
+
+void atualizarTela(char **scrn, int tamx, int tamy) {
+    for (int i = 0; i<tamx; i++){
+        for (int j=0; j<tamy; j++){
+            printf("%c", scrn[i][j]);
+            if (j == tamy - 1){
+                printf("\n");
+            }
+            
+        }
+    }
+}
+
+void inserePalavra(char *palavra, char **tela){
+
 }
 
 void distribuiPal(char **scrn, char **palavras, int linha, int col, int np){
